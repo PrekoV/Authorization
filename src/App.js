f9389bed01618d0561ab25b581b2335fa437acb7
vikapredko1999@gmail.com <vikapredko1999@gmail.com>
2019-01-22 11:20:34 +0300

    finish styles, fix the authorization

--- src/App.js
+++ src/App.js
@@ -12,33 +12,21 @@ class App extends Component {
 
 	componentDidMount = () => {
 		if (localStorage.getItem("hash")) {
-			//	let currentState = this.state
-			this.setState({ hash: localStorage.getItem("hash") })
-			console.log(localStorage.getItem("hash"))
-			API('POST', localStorage.getItem("login"), localStorage.getItem("pass"))
-				.then(json => {
-					this.setState({ hash: json.token.hash, title: 'Authorizated: ' + json.user.firstName, user: json.user })
-				})
 			API(null, null, null, localStorage.getItem("hash"), localStorage.getItem("userId"))
 				.then(json => {
 					console.log(json.user.firstName)
-					// this.setState({ user: json.user })
-					// this.setState({ title: 'You h' + json.user.id })
+					console.log(localStorage.getItem("hash"))
+					this.setState({ user: json.user, title: 'Authorizated: ' + json.user.firstName })
 				})
-			//currentState.title = 'You have been already authorized!'
-			//console.log(currentState)
-
-			//this.setState({ state: currentState })
 		}
 	}
 
 	auth = () => {
 		var login = document.getElementById("login").value
 		var pass = document.getElementById("pass").value
-
 		var POST = 'POST'
 
-		console.log(login + " " + pass)
+		console.log("Input values: " + login + " " + pass)
 
 		if (!localStorage.getItem("hash")) {
 			API(POST, login, pass)
@@ -49,16 +37,19 @@ class App extends Component {
 				}).then(state => {
 					localStorage.setItem("hash", state.hash)
 					localStorage.setItem("userId", state.user.id)
-					localStorage.setItem("login", state.user.email)
-					localStorage.setItem("pass", document.getElementById("pass").value)
 					document.getElementById("login").value = ''
 					document.getElementById("pass").value = ''
 					console.log(state)
-					return this.state
-				}).then(state => {
-					return API(null, null, null, state.hash)
-				}).then(json => {
-					console.log(json)
+
+					// проверка: действительно ли пользователь в сети?
+					// при удачной авторизации можно просматривать данные других пользователей
+					//
+					//	return this.state
+					// }).then(state => {
+					// 	return API(null, null, null, state.hash, state.user.id)
+					// }).then(json => {
+					// 	console.log(json)
+
 				}).catch(error => {
 					console.log(error)
 					this.setState({ title: 'Invalid username or password' })
@@ -81,20 +72,25 @@ class App extends Component {
 			<div className="App">
 				<div className="access">
 					<div className="header">
-						<h1>{this.state.title}</h1>
+						<h3>{this.state.title}</h3>
 					</div>
 					<form action="">
-						<span className="text">Email</span>
-						<input type="email" placeholder="Email" id="login" />
-						<br />
-						<span className="text">Password</span>
-						<input type="password" placeholder="Password" id="pass" />
+						<div className="block">
+							<span className="text">Email</span>
+							<input type="email" placeholder="Email" id="login" />
+						</div>
+						<div className="block">
+							<span className="text">Password</span>
+							<input type="password" placeholder="Password" id="pass" />
+						</div>
 					</form>
-					<button className="submit" onClick={() => this.auth()}>Submit</button>
-					<button className={`submit ${this.state.hash === '' ? 'passive' : ''}`}
-						onClick={() => this.clearLocalStorage()}>
-						Log out
-					</button>
+					<div className="btnWrapper">
+						<button className="submit" onClick={() => this.auth()}>Submit</button>
+						<button className={`submit ${localStorage.getItem("hash") || this.state.hash ? '' : 'passive'}`}
+							onClick={() => this.clearLocalStorage()}>
+							Log out
+						</button>
+					</div>
 				</div>
 			</div>
 		);
