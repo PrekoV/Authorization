f8f10a54cfda889d19aecc1413abf5f00fc7b300
vikapredko1999@gmail.com <vikapredko1999@gmail.com>
2019-01-22 17:47:02 +0300

    fix request

--- src/App.js
+++ src/App.js
@@ -1,61 +1,87 @@
 import React, { Component } from 'react';
 import './App.css'
 import API from './api'
+const emailPattern = /^ [a - zA - Z0 - 9.!#$ %& '*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/igm;
+
+
 
 class App extends Component {
 
 	state = {
-		hash: '',
 		title: '',
 		user: {},
-		btn: ''
+		btn: '',
+		login: '',
+		pass: '',
+		displayInput: ''
 	}
 
 	componentDidMount = () => {
 		if (localStorage.getItem("hash")) {
-			API(null, null, null, localStorage.getItem("hash"), localStorage.getItem("userId"))
+			const hash = localStorage.getItem("hash")
+			const id = localStorage.getItem("userId")
+			API('GET', 'user/' + id)
 				.then(json => {
 					console.log(json.user.firstName)
-					console.log(localStorage.getItem("hash"))
-					this.setState({ user: json.user, title: 'Authorizated: ' + json.user.firstName, btn: 'Log out' })
+					console.log(hash)
+					this.setState({
+						user: json.user,
+						title: 'You are already authorizated',
+						btn: 'Log out',
+						displayInput: 'none'
+					})
 				})
 		} else {
-			this.setState({ title: 'Log in', btn: 'Submit' })
+			this.setState({ title: 'Log in', btn: 'Submit', displayInput: 'flex' })
 		}
 	}
 
-	auth = () => {
-		var login = document.getElementById("login").value
-		var pass = document.getElementById("pass").value
-		var POST = 'POST'
+	auth = e => {
+		e.preventDefault()
+
+		const login = this.state.login
+		const pass = this.state.pass
+		const POST = 'POST'
+		const hash = localStorage.getItem("hash")
 
 		console.log("Input values: " + login + " " + pass)
 
-		if (!localStorage.getItem("hash")) {
-			API(POST, login, pass)
+		if (hash === null || hash === '') {
+			API(POST, 'token', { email: login, password: pass })
 				.then(json => {
-					this.setState({ title: 'Welcome, ' + json.user.firstName + "!", user: json.user, btn: 'Log out' })
+					this.setState({
+						title: 'Welcome, ' + json.user.firstName + "!",
+						user: json.user,
+						btn: 'Log out',
+						displayInput: 'none',
+						login: '',
+						pass: ''
+					})
 					localStorage.setItem("hash", json.token.hash)
 					localStorage.setItem("userId", json.user.id)
 					document.getElementById("login").value = ''
 					document.getElementById("pass").value = ''
-
-					// проверка: действительно ли пользователь в сети?
-					// при удачной авторизации можно просматривать данные других пользователей
-					//
-					//	return this.state
-					// }).then(state => {
-					// 	return API(null, null, null, state.hash, state.user.id)
-					// }).then(json => {
-					// 	console.log(json)
-
+				}, err => {
+					console.log(err.message)
 				}).catch(error => {
 					console.log(error)
 					this.setState({ title: 'Invalid username or password' })
 				})
 		} else {
 			localStorage.clear()
-			this.setState({ hash: '', title: 'Log in', btn: 'Submit' })
+			this.setState({ hash: '', title: 'Log in', btn: 'Submit', displayInput: 'flex', })
+		}
+
+	}
+
+	setChange = e => {
+		if (e.target.name === 'email') {
+			//if (emailPattern.test(e.target.value)) {
+			if (e.target.value.search('@') !== -1 && e.target.value.search('.') !== -1) {
+				this.setState({ login: e.target.value })
+			}
+		} else {
+			this.setState({ pass: e.target.value })
 		}
 	}
 
@@ -66,19 +92,24 @@ class App extends Component {
 					<div className="header">
 						<h3>{this.state.title}</h3>
 					</div>
-					<form action="">
-						<div className="block">
-							<span className="text">Email</span>
-							<input type="email" placeholder="Email" id="login" />
+					<form action="" onSubmit={this.auth}>
+						<div className="block" style={{ display: this.state.displayInput }}>
+							<span className="text" >Email</span>
+							<input type="email" name="email" placeholder="Email" id="login" onChange={this.setChange} />
 						</div>
-						<div className="block">
+						<div className={`info ${this.state.displayInput === 'none' ? 'showInfo' : ''}`}>
+							name: {this.state.user.firstName} {this.state.user.lastName}
+						</div>
+						<div className="block" style={{ display: this.state.displayInput }}>
 							<span className="text">Password</span>
-							<input type="password" placeholder="Password" id="pass" />
+							<input type="password" name="pass" placeholder="Password" id="pass" onChange={this.setChange} />
+						</div>
+						<div className={`info ${this.state.displayInput === 'none' ? 'showInfo' : ''}`}> age: {this.state.user.age}</div>
+						<div className="btnWrapper">
+							<button className="submit">{this.state.btn}</button>
 						</div>
 					</form>
-					<div className="btnWrapper">
-						<button className="submit" onClick={() => this.auth()}>{this.state.btn}</button>
-					</div>
+
 				</div>
 			</div>
 		);
--- src/api.js
+++ src/api.js
@@ -1,38 +1,25 @@
-const api = (method, email, password, hash, id) => {
+const api = (method, url, body) => {
+
+    const hash = localStorage.getItem("hash");
 
     let options = {
-        method: 'GET' //в документации хттп протокола method по умолчанию get
-    }
-    if (method) {
-        options.method = method
-    }
-    if (hash) {
-        options.headers = {
+        method: method,
+        headers: {
             'accept': 'application/json',
-            'authorization': hash
-        }
-        //options.headers = JSON.stringify({ authorization: hash })
+            'authorization': hash,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
     }
-    if (email && password) {
-        options.body = JSON.stringify({ email: email, password: password })
-        options.headers = {
-            'Content-Type': 'application/json' // postman
-        }
-    }
-    if (id) {
-        return fetch(`http://std.powercode.pro:5000/api/v1/${method === 'POST' ? 'token' : 'user/' + id}`, options)
+
+    try {
+        return fetch(`http://std.powercode.pro:5000/api/v1/${url}`, options)
             .then(res => {
                 return res.json()
             })
-    } else {
-        return fetch(`http://std.powercode.pro:5000/api/v1/${method === 'POST' ? 'token' : 'user/'}`, options)
-            .then(res => {
-                return res.json()
-            })
+    } catch (e) {
+        console.log(e.message)
     }
-    // .then(json => { //если без return
-    //     console.log(json)
-    // })
 }
 
 export default api
